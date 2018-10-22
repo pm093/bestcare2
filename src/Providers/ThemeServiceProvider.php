fa783f4857ac10f1db56bbd301741f1fe2bb3e3b
pm093 <piotr.marcinkowski101@gmail.com>
2018-10-22 20:08:41 +0200

    init

---
+++ src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+ 
+namespace Theme\Providers;
+ 
+use IO\Extensions\Functions\Partial;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
+ 
+class ThemeServiceProvider extends ServiceProvider
+{
+ 
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+ 
+	}
+}
